ef7a84bef3db81ba06815420354062e151f11405
Kristof Van Tricht <kristof.vantricht@vito.be>
2023-10-05 15:19:20 +0200

    Delete deprecated files

--- src/worldcereal/config/__init__.py
+++
@@ -1,7 +0,0 @@
-from .settings import (get_job_options,
-                       get_processing_options,
-                       get_collection_options,
-                       MODEL_PATH)
-
-__all__ = ['get_job_options', 'get_processing_options',
-           'get_collection_options', 'MODEL_PATH']
