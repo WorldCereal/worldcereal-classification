f112b82fcf28cf95a6876cc555661571edbcbc86
DEGERICJ <jeroen.degerickx@vito.be>
2024-05-24 13:55:07 +0200

    introduced placeholders for different steps

--- scripts/system_demo/system_v1.py
+++ scripts/system_demo/system_v1.py
@@ -1,4 +1,19 @@
 # %%
+
+# ADD WORLDCEREAL LOGO in markdown
+# ![](./WorldCereal_logo.png)
+# ADD ESA LOGO
+# ![](./ESA_logo.png)
+
+# ADD TITLE: WorldCereal System v1 Demo
+
+# DATE: ...
+
+# AUTHORS: ...
+
+# ADD consortium logos
+# ![](./Consortium_logos.png)
+
 '''
 This notebook contains a demo of the WorldCereal system v1
 and all its functionalities.
@@ -16,6 +31,10 @@ Content:
     (and/or the default cropland model)
 
 '''
+
+# ADD TABLE OF CONTENTS USING MARKDOWN
+# Notebook outline:
+# - [1. Introduction](#1.-Introduction)
 
 # %%
 # IMPORTS
@@ -50,12 +69,11 @@ headers = {
     'Authorization': f'{tokentype} {accessToken}',
 }
 
-
 # %% Define a region of interest
 m, dc = get_ui_map()
 m
 
-# Now use the interactive map to draw a rectangle around the region of interest
+# Now use the interactive map to draw a rectangle around your region of interest...
 
 # %%
 # retrieve bounding box from drawn rectangle
@@ -72,17 +90,34 @@ print(f'Your area of interest: {bbox}')
 # Generate default cropland and crop type products for
 # the region of interest
 
-# NEEDS INFERENCE RUN
+# Perform inference run with the default cropland model
+start_date = '2021-01-01'
+end_date = '2021-12-31'
+aoi = bbox
+cropland_result = run_inference(aoi, start_date, end_date, product='cropland')
+# download and visualize map
+
+# Perform inference run with default crop type model
+croptype_result = run_inference(aoi, start_date, end_date, product='maize')
+# download and visualize map
+
+# use the cropland product to mask the previously generated custom crop type product
+croptype_fin = mask_product(croptype_result, cropland_result)
 
 
 # %%
 # Exploring available reference data in the RDM
+
+# Demo on RDM API Phase I --> https://github.com/WorldCereal/ewoc_rdm_demo_api/blob/8548a65e04350a9a881fc4d78f54a170d6809c2c/rdmApiDemo.ipynb#L22
+# Functionalities of RDM API Phase II --> https://ewoc-rdm-api.iiasa.ac.at/swagger/index.html
+
+# Check full list of available collections
 collectionResponse = requests.get(f'{RDM_API}/collections', headers=headers)
 collections = collectionResponse.json()
 col_ids = [x['collectionId'] for x in collections['items']]
 print(f'Available collections: {col_ids}')
 
-# Now we check which collections intersect our AOI
+# Now we check which collections intersect with our AOI
 bbox_str = f'Bbox={bbox[0]}&Bbox={bbox[1]}&Bbox={bbox[2]}&Bbox={bbox[3]}'
 colSearchUrl = f'{RDM_API}/collections/search?{bbox_str}'
 colSearchResponse = requests.get(colSearchUrl, headers=headers)
@@ -90,13 +125,18 @@ test = colSearchResponse.json()
 
 
 # %%
+# Contributing reference data to RDM
 
+# Here we should have a function that allows a user to upload a zip file containing the reference data
+# If possible, harmonization should be done on the server side, not by the user
+
+# check whether data has been successfully ingested...
 
 # %%
-# --> this should return the selected points as a geojson file/object...
+# Now that our reference data has been added, we launch a request to extract all reference points and polygons intersecting our region of interest
+# --> this should return the selected points/polygons as a geojson file/object (one per collection)
 
-
-# %%
+# Next, we convert polygons to points (by taking the centroid) and combine all reference data into one geoparquet file
 
 
 # %%
@@ -115,18 +155,18 @@ training_df = get_training_data(points)
 # %%
 # Train catboost model
 
+link_to_model = train_catboost_model(training_df)
+
+# Upload model to artifactory (should be automatically done by openeo)
+
 
 # %%
 # Perform inference run with the newly trained model
 
-link_to_model = ''
-aoi =
-start_date =
-end_date =
+aoi = bbox
+start_date = '2021-03-01'
+end_date = '2021-10-01'
 
+result = run_inference(aoi, start_date, end_date, model=link_to_model)
 
-# %%
-# Perform inference run with the default cropland model
-
-
-# use the cropland product to mask the previously generated custom crop type product
+# --> https://github.com/WorldCereal/worldcereal-classification/blob/kvt_mvp_inferenceUDF/minimal_wc_presto/backend_inference_example_openeo.ipynb
