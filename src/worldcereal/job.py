f4f83a09b10eeb3c86a9c6cadd9ed4056849d820
Kristof Van Tricht <kristof.vantricht@vito.be>
2024-06-21 17:09:36 +0200

    Add croptype inference

--- src/worldcereal/job.py
+++ src/worldcereal/job.py
@@ -14,6 +14,36 @@ from worldcereal.openeo.preprocessing import worldcereal_preprocessed_inputs_gfm
 ONNX_DEPS_URL = "https://artifactory.vgt.vito.be/artifactory/auxdata-public/openeo/onnx_dependencies_1.16.3.zip"
 
 
+PRODUCT_SETTINGS = {
+    "cropland": {
+        "features": {
+            "extractor": PrestoFeatureExtractor,
+            "parameters": {
+                "rescale_s1": False,  # Will be done in the Presto UDF itself!
+                "presto_model_url": "https://artifactory.vgt.vito.be/artifactory/auxdata-public/worldcereal-minimal-inference/presto.pt",  # NOQA
+            },
+        },
+        "classification": {
+            "classifier": CroplandClassifier,
+            "parameters": {"classifier_url": ""},
+        },
+    },
+    "croptype": {
+        "features": {
+            "extractor": PrestoFeatureExtractor,
+            "parameters": {
+                "rescale_s1": False,  # Will be done in the Presto UDF itself!
+                "presto_model_url": "https://artifactory.vgt.vito.be/artifactory/auxdata-public/worldcereal/models/PhaseII/presto-ss-wc-ft-ct-30D_test.pt",  # NOQA
+            },
+        },
+        "classification": {
+            "classifier": CroplandClassifier,  # TODO: update to croptype classifier
+            "parameters": {"classifier_url": ""},
+        },
+    },
+}
+
+
 def generate_map(
     spatial_extent: BoundingBoxExtent,
     temporal_extent: TemporalContext,
@@ -37,6 +67,9 @@ def generate_map(
 
     """
 
+    if product not in PRODUCT_SETTINGS.keys():
+        raise ValueError(f"Product {product} not supported.")
+
     # Connect to openeo
     connection = openeo.connect(
         "https://openeo.creo.vito.be/openeo/"
@@ -51,14 +84,10 @@ def generate_map(
     )
 
     # Run feature computer
-    presto_parameters = {
-        "rescale_s1": False,  # Will be done in the Presto UDF itself!
-    }
-
     features = apply_feature_extractor(
-        feature_extractor_class=PrestoFeatureExtractor,
+        feature_extractor_class=PRODUCT_SETTINGS[product]["features"]["extractor"],
         cube=inputs,
-        parameters=presto_parameters,
+        parameters=PRODUCT_SETTINGS[product]["features"]["parameters"],
         size=[
             {"dimension": "x", "unit": "px", "value": 100},
             {"dimension": "y", "unit": "px", "value": 100},
@@ -69,20 +98,13 @@ def generate_map(
         ],
     )
 
-    if product == "cropland":
-        # initiate default cropland model
-        model_inference_class = CroplandClassifier
-        model_inference_parameters = {}
-    else:
-        raise ValueError(f"Product {product} not supported.")
-
     if format not in ["GTiff", "NetCDF"]:
         raise ValueError(f"Format {format} not supported.")
 
     classes = apply_model_inference(
-        model_inference_class=model_inference_class,
+        model_inference_class=PRODUCT_SETTINGS[product]["classification"]["classifier"],
         cube=features,
-        parameters=model_inference_parameters,
+        parameters=PRODUCT_SETTINGS[product]["classification"]["parameters"],
         size=[
             {"dimension": "x", "unit": "px", "value": 100},
             {"dimension": "y", "unit": "px", "value": 100},
@@ -108,4 +130,44 @@ def generate_map(
             "executor-memoryOverhead": "12g",
             "udf-dependency-archives": [f"{ONNX_DEPS_URL}#onnx_deps"],
         },
+    )
+
+
+def collect_inputs(
+    spatial_extent: BoundingBoxExtent,
+    temporal_extent: TemporalContext,
+    backend_context: BackendContext,
+    output_path: Union[Path, str],
+):
+    """Function to retrieve preprocessed inputs that are being
+    used in the generation of WorldCereal products.
+
+    Args:
+        spatial_extent (BoundingBoxExtent): spatial extent of the map
+        temporal_extent (TemporalContext): temporal range to consider
+        backend_context (BackendContext): backend to run the job on
+        output_path (Union[Path, str]): output path to download the product to
+
+    Raises:
+        ValueError: if the product is not supported
+
+    """
+
+    # Connect to openeo
+    connection = openeo.connect(
+        "https://openeo.creo.vito.be/openeo/"
+    ).authenticate_oidc()
+
+    # Preparing the input cube for the inference
+    inputs = worldcereal_preprocessed_inputs_gfmap(
+        connection=connection,
+        backend_context=backend_context,
+        spatial_extent=spatial_extent,
+        temporal_extent=temporal_extent,
+    )
+
+    inputs.execute_batch(
+        outputfile=output_path,
+        out_format="NetCDF",
+        job_options={"driver-memory": "4g", "executor-memoryOverhead": "4g"},
     )
--- src/worldcereal/openeo/inference.py
+++ src/worldcereal/openeo/inference.py
@@ -80,3 +80,89 @@ class CroplandClassifier(ModelInference):
         )
 
         return classification
+
+
+class CroptypeClassifier(ModelInference):
+    """Multi-class crop classifier using ONNX to load a catboost model.
+
+    The classifier use the embeddings computed from the Presto Feature
+    Extractor.
+
+    Interesting UDF parameters:
+    - classifier_url: A public URL to the ONNX classification model. Default is
+      the public Presto model.
+    """
+
+    import numpy as np
+
+    CATBOOST_PATH = "https://artifactory.vgt.vito.be/artifactory/auxdata-public/worldcereal/models/PhaseII/presto-ss-wc-ft-ct-30D_test_CROPTYPE9.onnx"  # NOQA
+
+    def __init__(self):
+        super().__init__()
+
+        self.onnx_session = None
+
+    def dependencies(self) -> list:
+        return []  # Disable the dependencies from PIP install
+
+    def output_labels(self) -> list:
+        return ["classification", "probability"]
+
+    def predict(self, features: np.ndarray) -> np.ndarray:
+        """
+        Predicts labels using the provided features array.
+        """
+        import numpy as np
+
+        if self.onnx_session is None:
+            raise ValueError("Model has not been loaded. Please load a model first.")
+
+        # Prepare input data for ONNX model
+        outputs = self.onnx_session.run(None, {"features": features})
+
+        # Apply LUT: TODO:this needs an update!
+        LUT = {
+            "barley": 1,
+            "maize": 2,
+            "millet_sorghum": 3,
+            "other_crop": 4,
+            "rapeseed_rape": 5,
+            "soy_soybeans": 6,
+            "sunflower": 7,
+            "wheat": 8,
+        }
+
+        # Extract classes as INTs and probability of winning class values
+        labels = np.zeros((len(outputs[0]),), dtype=np.uint16)
+        probabilities = np.zeros((len(outputs[0]),), dtype=np.uint8)
+        for i, (label, prob) in enumerate(zip(outputs[0], outputs[1])):
+            labels[i] = LUT[label]
+            probabilities[i] = int(prob[label] * 100)
+
+        return np.stack([labels, probabilities], axis=0)
+
+    def execute(self, inarr: xr.DataArray) -> xr.DataArray:
+        classifier_url = self._parameters.get("classifier_url", self.CATBOOST_PATH)
+
+        # shape and indices for output ("xy", "bands")
+        x_coords, y_coords = inarr.x.values, inarr.y.values
+        inarr = inarr.transpose("bands", "x", "y").stack(xy=["x", "y"]).transpose()
+
+        self.onnx_session = self.load_ort_session(classifier_url)
+
+        # Run catboost classification
+        self.logger.info("Catboost classification with input shape: %s", inarr.shape)
+        classification = self.predict(inarr.values)
+        self.logger.info("Classification done with shape: %s", inarr.shape)
+
+        classification = xr.DataArray(
+            classification.reshape((2, len(x_coords), len(y_coords))),
+            dims=["bands", "x", "y"],
+            coords={
+                "bands": ["classification", "probability"],
+                "x": x_coords,
+                "y": y_coords,
+            },
+        )
+
+        return classification
