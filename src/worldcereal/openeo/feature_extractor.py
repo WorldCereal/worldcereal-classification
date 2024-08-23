1a7d6a2b7f4629a4c73536c2057737f5123767d3
Kristof Van Tricht <kristof.vantricht@vito.be>
2024-08-23 11:12:53 +0200

    #55 Immediately map to presto band names

--- src/worldcereal/openeo/feature_extractor.py
+++ src/worldcereal/openeo/feature_extractor.py
@@ -26,21 +26,21 @@ class PrestoFeatureExtractor(PatchFeatureExtractor):
     DEPENDENCY_NAME = "worldcereal_deps.zip"
 
     GFMAP_BAND_MAPPING = {
-        "S2-L2A-B02": "B02",
-        "S2-L2A-B03": "B03",
-        "S2-L2A-B04": "B04",
-        "S2-L2A-B05": "B05",
-        "S2-L2A-B06": "B06",
-        "S2-L2A-B07": "B07",
-        "S2-L2A-B08": "B08",
+        "S2-L2A-B02": "B2",
+        "S2-L2A-B03": "B3",
+        "S2-L2A-B04": "B4",
+        "S2-L2A-B05": "B5",
+        "S2-L2A-B06": "B6",
+        "S2-L2A-B07": "B7",
+        "S2-L2A-B08": "B8",
         "S2-L2A-B8A": "B8A",
         "S2-L2A-B11": "B11",
         "S2-L2A-B12": "B12",
         "S1-SIGMA0-VH": "VH",
         "S1-SIGMA0-VV": "VV",
-        "COP-DEM": "DEM",
-        "AGERA5-TMEAN": "temperature-mean",
-        "AGERA5-PRECIP": "precipitation-flux",
+        "COP-DEM": "altitude",
+        "AGERA5-TMEAN": "temperature_2m",
+        "AGERA5-PRECIP": "total_precipitation",
     }
 
     @functools.lru_cache(maxsize=6)
