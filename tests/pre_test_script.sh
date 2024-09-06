36e596a55c4ad8df312d20225cc4cd5939d0de99
Kristof Van Tricht <kristof.vantricht@vito.be>
2024-09-06 16:00:40 +0200

    Moved pre_test_script.sh

---
+++ tests/pre_test_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install openeo-gfmap
+GFMAP_URL="https://github.com/Open-EO/openeo-gfmap.git"
+su - jenkins -c "git clone $GFMAP_URL"
+cd openeo-gfmap || { echo "Directory not found! Exiting..."; exit 1; }
+su - jenkins -c "pip install ."
+cd ..
+
+# Install
+PRESTO_URL="https://github.com/WorldCereal/presto-worldcereal.git"
+su - jenkins -c "git clone --branch updated-inferencedatasets $PRESTO_URL"
+cd presto-worldcereal || { echo "Directory not found! Exiting..."; exit 1; }
+su - jenkins -c "pip install ."
+cd ..
