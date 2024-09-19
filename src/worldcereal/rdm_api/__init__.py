a4217ca2e3ef2e86e24da6c2faed94b351599e68
Vincent Verelst <vincent.verelst@vito.be>
2024-09-19 14:46:23 +0200

    Created new rdm_api module #125

--- src/worldcereal/rdm_api/__init__.py
+++ src/worldcereal/rdm_api/__init__.py
@@ -1,6 +1,6 @@
 """This sub-module contains utilitary function and tools for worldcereal-classification"""
 
-from worldcereal.utils.rdm_interaction import query_ground_truth
+from worldcereal.rdm_api.rdm_interaction import query_ground_truth
 
 __all__ = [
     "query_ground_truth",
