be1538f20285695521327841310773120eddab0c
Kristof Van Tricht <kristof.vantricht@vito.be>
2024-10-10 17:12:02 +0200

    Updated Presto embeddings computation

--- scripts/spark/compute_presto_features.py
+++ scripts/spark/compute_presto_features.py
@@ -1,84 +1,16 @@
 import glob
 from pathlib import Path
-from typing import List, Union
+from typing import Union
 
 import pandas as pd
 import torch
 from loguru import logger
-from presto.dataset import WorldCerealLabelledDataset
-from presto.masking import MaskParamsNoDw
 from presto.presto import Presto
 from presto.utils import device, process_parquet
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-
-class WorldCerealInputsDataset(WorldCerealLabelledDataset):
-
-    def __init__(
-        self,
-        dataframe: pd.DataFrame,
-        task_type: str = "cropland",
-        croptype_list: List = [],
-        return_hierarchical_labels: bool = False,
-        augment: bool = False,
-        mask_ratio: float = 0.0,
-        repeats: int = 1,
-    ):
-        dataframe = dataframe.loc[~dataframe.LANDCOVER_LABEL.isin(self.FILTER_LABELS)]
-
-        self.task_type = task_type
-        self.croptype_list = croptype_list
-        self.return_hierarchical_labels = return_hierarchical_labels
-        self.augment = augment
-        if augment:
-            logger.info(
-                "Augmentation is enabled. \
-    The horizontal jittering of the selected window will be performed."
-            )
-        self.mask_ratio = mask_ratio
-        self.mask_params = MaskParamsNoDw(
-            (
-                "group_bands",
-                "random_timesteps",
-                "chunk_timesteps",
-                "random_combinations",
-            ),
-            mask_ratio,
-        )
-
-        super(WorldCerealLabelledDataset, self).__init__(dataframe)
-        logger.info(f"Original dataset size: {len(dataframe)}")
-
-        self.indices = []
-        for _ in range(repeats):
-            self.indices += [i for i in range(len(self.df))]
-
-        logger.info(f"Dataset size after {repeats} repeats: {len(self.indices)}")
-
-    def __iter__(self):
-        for idx in self.indices:
-            yield self.__getitem__(idx)
-
-    def __getitem__(self, idx):
-
-        # Get the sample
-        sample = super().__getitem__(idx)
-        row = self.df.iloc[self.indices[idx], :]
-
-        attrs = [
-            "WORLDCOVER-LABEL-10m",
-            "lat",
-            "lon",
-            "CROPTYPE_LABEL",
-            "LANDCOVER_LABEL",
-            "POTAPOV-LABEL-10m",
-            "location_id",
-            "ref_id",
-            "sample_id",
-        ]
-
-        return sample + (row[attrs].to_dict(),)
+from worldcereal.train.dataset import WorldCerealTrainingDataset
 
 
 def embeddings_from_parquet_file(
@@ -128,7 +60,7 @@ def embeddings_from_parquet_file(
 
     # Create dataset and dataloader
     logger.info("Making data loader ...")
-    ds = WorldCerealInputsDataset(
+    ds = WorldCerealTrainingDataset(
         df,
         task_type="cropland",
         augment=True,
@@ -257,14 +189,15 @@ if __name__ == "__main__":
     spark = True
     localspark = False
     debug = False
-    sample_repeats = 3
+    sample_repeats = 1
     mask_ratio = 0.25
     valid_date_as_token = False
     presto_dir = Path("/vitodata/worldcereal/presto/finetuning")
     presto_model = (
         presto_dir
-        / "presto-ss-wc-ft-ct_100epochs_30D_random_CROPLAND2_time-token=none_balance=True_augment=True_2017=True.pt"
+        / "presto-ss-wc-ft-ct_cropland_CROPLAND2_30D_random_time-token=none_balance=True_augment=True.pt"
     )
+    identifier = ""
 
     if spark:
         from worldcereal.utils.spark import get_spark_context
@@ -278,11 +211,11 @@ if __name__ == "__main__":
 
     if debug:
         outfile = baseoutdir / (
-            f"training_df_{presto_model.stem}_presto-worldcereal_DEBUG.parquet"
+            f"training_df_{presto_model.stem}_presto-worldcereal{identifier}_DEBUG.parquet"
         )
     else:
         outfile = baseoutdir / (
-            f"training_df_{presto_model.stem}_presto-worldcereal.parquet"
+            f"training_df_{presto_model.stem}_presto-worldcereal{identifier}.parquet"
         )
 
     if not infile.exists():
