126366ba8ce033ed48a0ebbc5fd46ce96f4fc059
jdegerickx <jeroen.degerickx@vito.be>
2024-11-25 13:07:52 +0100

    fix import ordering

--- src/worldcereal/rdm_api/__init__.py
+++ src/worldcereal/rdm_api/__init__.py
@@ -1,7 +1,7 @@
 """This sub-module contains utilitary function and tools for worldcereal-classification"""
 
-from .rdm_interaction import RdmInteraction
 from .rdm_collection import RdmCollection
+from .rdm_interaction import RdmInteraction
 
 __all__ = [
     "RdmInteraction",
