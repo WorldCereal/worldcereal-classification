7623f6e140b3b937fb36bbb438da4be90876cce9
jdegerickx <jeroen.degerickx@vito.be>
2024-12-11 16:52:34 +0100

    first version of refactored query_extractions, including public and private

--- src/worldcereal/utils/refdata.py
+++ src/worldcereal/utils/refdata.py
@@ -13,8 +13,11 @@ from loguru import logger
 from openeo_gfmap import TemporalContext
 from shapely.geometry import Polygon
 
+from openeo_gfmap.manager.job_splitters import load_s2_grid
+
 from worldcereal.data import croptype_mappings
 from worldcereal.utils.legend import download_latest_legend_from_artifactory
+from worldcereal.rdm_api import RdmInteraction
 
 
 def get_class_mappings() -> Dict:
@@ -29,6 +32,214 @@ def get_class_mappings() -> Dict:
         CLASS_MAPPINGS = json.load(f)
 
     return CLASS_MAPPINGS
+
+
+def query_extractions(
+    bbox_poly: Polygon,
+    buffer: int = 250000,
+    include_public: bool = True,
+    private_extraction_dir: Path = None,
+) -> pd.DataFrame:
+    """Method to query the WorldCereal global database of pre-extracted input data for a given area.
+    You can query both public and private extractions.
+    
+    Parameters
+    ----------
+    bbox_poly : Polygon
+        bounding box of the area to make the query for. Expected to be in WGS84 coordinates.
+    buffer : int, optional
+        buffer (in meters) to apply to the requested area, by default 250000
+    include_public: bool, optional
+        include public extractions in the query, by default True
+    private_extraction_dir: Path, optional
+        path to the directory containing private extractions, by default None,
+        meaning no private extractions will be included.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing the extractions matching the request.
+    """
+    
+    # Prepare the spatial extent
+    logger.info(f"Applying a buffer of {int(buffer/1000)} km to the selected area ...")
+
+    bbox_poly = (
+        gpd.GeoSeries(bbox_poly, crs="EPSG:4326")
+        .to_crs(epsg=3785)
+        .buffer(buffer, cap_style="square", join_style="mitre")
+        .to_crs(epsg=4326)[0]
+    )
+
+    xmin, ymin, xmax, ymax = bbox_poly.bounds
+    bbox_poly = Polygon([(xmin, ymin), (xmin, ymax), (xmax, ymax), (xmax, ymin)])
+    
+    # Query the public extractions
+    if include_public:
+        public_df = _query_public_extractions(bbox_poly)
+    else:
+        public_df = pd.DataFrame()
+    
+    # Query the private extractions
+    if private_extraction_dir:
+        private_df = _query_private_extractions(
+            private_extraction_dir, bbox_poly
+        )
+    else:
+        private_df = pd.DataFrame()
+
+    # Combine the public and private extractions
+    dfs = [public_df, private_df]
+    dfs = [df for df in dfs if df.empty is False]
+    if not dfs:
+        logger.error(
+            "No extractions found that intersect with the selected area, cannot continue."
+        )
+        raise ValueError(
+            "No extractions found  that intersect with the selected area, cannot continue."
+        )
+    df_raw = pd.concat(dfs, ignore_index=True)
+    
+    # Check if the data contains only one class
+    if df_raw["ewoc_code"].nunique() == 1:
+        logger.error(
+            f"Queried data contains only one class: {df_raw['ewoc_code'].unique()[0]}. Cannot train a model with only one class."
+        )
+        raise ValueError(
+            "Queried data contains only one class. Cannot train a model with only one class."
+        )
+    
+    return df_raw
+    
+    
+def _query_public_extractions(
+    bbox_poly: Polygon,
+) -> pd.DataFrame:
+    """Method to query the WorldCereal public database of pre-extracted input data for a given area.
+    
+    Parameters
+    ----------
+    bbox_poly : Polygon
+        bounding box of the area to make the query for. Expected to be in WGS84 coordinates.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing the extractions matching the request.
+    """
+    
+    # First we query the RDM to get a list of overlapping datasets
+    rdm = RdmInteraction()
+    collections = rdm.get_collections(geometry=bbox_poly)
+    ref_ids = [collection.id for collection in collections]
+    
+    if not ref_ids:
+        logger.warning(
+            "No public datasets found in the WorldCereal Reference Data Module that intersect with the selected area."
+        )
+        return pd.DataFrame()
+    
+    logger.info(
+        f"Found {len(ref_ids)} public datasets in WorldCereal Reference Data Module that intersect with the selected area."
+    )
+    # Query the public extraction
+    logger.info(
+        "Querying WorldCereal public extractions database (this can take a while) ..."
+    )
+    
+    #TODO: construct the query...
+    main_query = ""
+    
+    # Execute the query
+    db = duckdb.connect()
+    db.sql("INSTALL spatial")
+    db.load_extension("spatial")
+    
+    public_df_raw = db.sql(main_query).df()
+
+    if public_df_raw.empty:
+        logger.warning(
+            f"No samples from the WorldCereal public extractions database fall into the selected area."
+        )
+
+    return public_df_raw
+
+
+def _query_private_extractions(
+    private_extraction_dir: Path,
+    bbox_poly: Polygon,
+ ) -> pd.DataFrame:
+    """Method to query private pre-extracted input data for a given area.
+    
+    Parameters
+    ----------
+    private_extraction_dir: Path
+        path to a local directory containing private extractions
+    bbox_poly : Polygon
+        bounding box of the area to make the query for. Expected to be in WGS84 coordinates.
+        
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing the extractions matching the request.
+    """
+    
+    # First we query the RDM to get a list of overlapping datasets
+    rdm = RdmInteraction()
+    collections = rdm.get_collections(geometry=bbox_poly,
+                                      include_private=True,
+                                      include_public=False)
+    ref_ids = [collection.id for collection in collections]
+    
+    if not ref_ids:
+        logger.warning(
+            "No private datasets found in the WorldCereal Reference Data Module that intersect with the selected area."
+        )
+        return pd.DataFrame()
+    
+    logger.info(
+        f"Found {len(ref_ids)} private datasets in WorldCereal Reference Data Module that intersect with the selected area."
+    )
+    
+    # Check which S2 tiles are intersecting with the selected area
+    s2_grid = load_s2_grid()
+    s2_tiles = s2_grid[s2_grid.intersects(bbox_poly)]
+    s2_tile_ids = s2_tiles["tile"].values
+    
+    # Now locate the associated parquet files in the private extraction directory
+    parquet_files = []
+    for ref_id in ref_ids:
+        search_dir = private_extraction_dir / ref_id
+        tile_dirs = [search_dir / tile_id for tile_id in s2_tile_ids]
+        for tile_dir in tile_dirs:
+            if tile_dir.exists():
+                parquet_files.extend(list(tile_dir.glob("**/point_extractions.geoparquet")))
+    
+    if not parquet_files:
+        logger.warning(
+            "No private extractions found in the selected area."
+        )
+        return pd.DataFrame()
+        
+    # Load the DuckDB spatial extension
+    conn = duckdb.connect()
+    conn.execute("INSTALL spatial; LOAD spatial;")
+    
+    # Convert the Shapely polygon to WKT
+    query_polygon_wkt = bbox_poly.wkt
+    
+    # Construct the query
+    query = f"""
+    SELECT *
+    FROM read_parquet({parquet_files})
+    WHERE ST_Intersects(ST_GeomFromWKT(geometry), ST_GeomFromText('{query_polygon_wkt}'))
+    """
+    
+    # Execute the query and convert results to a GeoDataFrame
+    result = conn.execute(query).fetchdf()
+    gdf = gpd.GeoDataFrame(result, geometry=gpd.GeoSeries.from_wkt(result['geometry']))
+    
+    return gdf
 
 
 def query_public_extractions(
