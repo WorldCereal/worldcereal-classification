c476f0460f7439c38b825ecb9f926feca92ff3c4
jdegerickx <jeroen.degerickx@vito.be>
2025-01-02 10:03:42 +0100

    added custom output projection

--- scripts/inference/cropland_mapping_udp.py
+++ scripts/inference/cropland_mapping_udp.py
@@ -24,11 +24,15 @@ temporal_extent = ["2020-01-01", "2020-12-31"]
 
 c = openeo.connect("openeofed.dataspace.copernicus.eu").authenticate_oidc()
 
+# This argument is optional and determines the projection of the output products
+target_projection = "EPSG:4326"
+
 classes = c.datacube_from_process(
     process_id="worldcereal_crop_extent",
     namespace="https://raw.githubusercontent.com/ESA-APEx/apex_algorithms/refs/heads/main/openeo_udp/worldcereal_crop_extent.json",
     temporal_extent=temporal_extent,
     spatial_extent=spatial_extent,
+    projection=target_projection,
 )
 
 ONNX_DEPS_URL = "https://artifactory.vgt.vito.be/artifactory/auxdata-public/openeo/onnx_dependencies_1.16.3.zip"
