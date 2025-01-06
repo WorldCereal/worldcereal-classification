c559ae188d53c9e74892b00bde3f3676b21a3f57
jdegerickx <jeroen.degerickx@vito.be>
2025-01-06 13:36:48 +0100

    updated example script

--- scripts/misc/legend.py
+++ scripts/misc/legend.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 from worldcereal.utils.legend import (
     delete_legend_file,
-    download_latest_legend_from_artifactory,
+    get_latest_legend_from_artifactory,
     upload_legend_csv_artifactory,
 )
 
@@ -14,16 +14,15 @@ if __name__ == "__main__":
 
     # Example usage
     srcpath = Path(
-        "/vitodata/worldcereal/data/legend/WorldCereal_LC_CT_legend_20241216.csv"
+        "/vitodata/worldcereal/data/legend/WorldCereal_LC_CT_legend_20241231.csv"
     )
-    date = "20241216"
-    download_path = Path("/vitodata/worldcereal/data/legend_v2")
+    date = "20241231"
 
     # Upload the legend to Artifactory
     link = upload_legend_csv_artifactory(srcpath, date)
 
     # Download the latest legend from Artifactory
-    download_latest_legend_from_artifactory(download_path)
+    legend = get_latest_legend_from_artifactory()
 
     # Delete the uploaded legend from Artifactory
     delete_legend_file(link)
