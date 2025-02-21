38cacc13f6a1eb0a32a3d71e02884f02825a73af
jdegerickx <jeroen.degerickx@vito.be>
2025-02-21 18:45:46 +0100

    changed udp link + re-organized script

--- scripts/inference/cropland_mapping_udp.py
+++ scripts/inference/cropland_mapping_udp.py
@@ -11,6 +11,8 @@
 
 import openeo
 
+###### USER INPUTS ######
+# Define the spatial and temporal extent
 spatial_extent = {
     "west": 622694.5968575787,
     "south": 5672232.857114074,
@@ -19,35 +21,38 @@ spatial_extent = {
     "crs": "EPSG:32631",
     "srs": "EPSG:32631",
 }
-
 temporal_extent = ["2020-01-01", "2020-12-31"]
-
-c = openeo.connect("openeofed.dataspace.copernicus.eu").authenticate_oidc()
 
 # This argument is optional and determines the projection of the output products
 target_projection = "EPSG:4326"
 
+# Here you can set custom job options, such as driver-memory, executor-memory, etc.
+# In case you would like to use the default options, set job_options to None.
+
+# job_options = {
+#     "driver-memory": "4g",
+#     "executor-memory": "2g",
+#     "python-memory": "3g",
+# }
+job_options = None
+
+###### END OF USER INPUTS ######
+
+# Connect to openeo backend
+c = openeo.connect("openeo.dataspace.copernicus.eu").authenticate_oidc()
+
+# Define the operations
 classes = c.datacube_from_process(
     process_id="worldcereal_crop_extent",
-    namespace="https://raw.githubusercontent.com/ESA-APEx/apex_algorithms/refs/heads/main/openeo_udp/worldcereal_crop_extent.json",
+    namespace="https://github.com/WorldCereal/worldcereal-classification/blob/worldcereal_crop_extent_v1.0.1/src/worldcereal/udp/worldcereal_crop_extent.json",
     temporal_extent=temporal_extent,
     spatial_extent=spatial_extent,
     projection=target_projection,
 )
 
-ONNX_DEPS_URL = "https://artifactory.vgt.vito.be/artifactory/auxdata-public/openeo/onnx_dependencies_1.16.3.zip"
-
-JOB_OPTIONS = {
-    "driver-memory": "4g",
-    "executor-memory": "2g",
-    "executor-memoryOverhead": "1g",
-    "python-memory": "3g",
-    "soft-errors": "true",
-    "udf-dependency-archives": [f"{ONNX_DEPS_URL}#onnx_deps"],
-}
-
+# Run the job
 job = classes.execute_batch(
     title="WorldCereal Crop Extent UDP test",
     out_format="GTiff",
-    job_options=JOB_OPTIONS,
+    job_options=job_options,
 )
