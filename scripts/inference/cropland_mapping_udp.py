4dd859f581774cdf477c8674b368a614b0b0cf89
jdegerickx <jeroen.degerickx@vito.be>
2025-02-25 10:57:54 +0100

    changed default spatial extent and added explanation on downloaded products

--- scripts/inference/cropland_mapping_udp.py
+++ scripts/inference/cropland_mapping_udp.py
@@ -5,7 +5,7 @@
     (using smooth_probabilities method).
 
     The user needs to manually download the resulting map through the OpenEO Web UI:
-    https://openeofed.dataspace.copernicus.eu/
+    https://openeo.dataspace.copernicus.eu/
     Or use the openeo API to fetch the resulting map.
     """
 
@@ -14,13 +14,16 @@ import openeo
 ###### USER INPUTS ######
 # Define the spatial and temporal extent
 spatial_extent = {
-    "west": 622694.5968575787,
-    "south": 5672232.857114074,
-    "east": 623079.000934101,
-    "north": 5672519.995940826,
-    "crs": "EPSG:32631",
-    "srs": "EPSG:32631",
+    "west": 3.809252,
+    "south": 51.232365,
+    "east": 3.833542,
+    "north": 51.245477,
+    "crs": "EPSG:4326",
+    "srs": "EPSG:4326",
 }
+
+# Temporal extent needs to consist of exactly twelve months,
+# always starting first day of the month and ending last day of the month.
 temporal_extent = ["2020-01-01", "2020-12-31"]
 
 # This argument is optional and determines the projection of the output products
@@ -44,7 +47,7 @@ c = openeo.connect("openeo.dataspace.copernicus.eu").authenticate_oidc()
 # Define the operations
 classes = c.datacube_from_process(
     process_id="worldcereal_crop_extent",
-    namespace="https://github.com/WorldCereal/worldcereal-classification/blob/worldcereal_crop_extent_v1.0.1/src/worldcereal/udp/worldcereal_crop_extent.json",
+    namespace="https://raw.githubusercontent.com/WorldCereal/worldcereal-classification/refs/tags/worldcereal_crop_extent_v1.0.1/src/worldcereal/udp/worldcereal_crop_extent.json",
     temporal_extent=temporal_extent,
     spatial_extent=spatial_extent,
     projection=target_projection,
@@ -56,3 +59,15 @@ job = classes.execute_batch(
     out_format="GTiff",
     job_options=job_options,
 )
+
+###### EXPLANATION OF RESULTS ######
+# Two GeoTiff images will be generated:
+# 1. "worldcereal-cropland-extent.tif": The original results of the WorldCereal cropland model
+# 2. "worldcereal-cropland-extent-postprocessed.tif": The results after applying the default post-processing,
+#   which includes a smoothing of the probabilities and relabelling of pixels according to the smoothed probabilities.
+
+# Each tif file contains 4 bands, all at 10m resolution:
+# - Band 1: Predicted cropland labels (0: non-cropland, 1: cropland)
+# - Band 2: Probability of the winning class (50 - 100)
+# - Band 3: Probability of the non-cropland class (0 - 100)
+# - Band 4: Probability of the cropland class (0 - 100)
