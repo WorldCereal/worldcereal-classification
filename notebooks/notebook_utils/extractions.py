c9100d82278f0eddc436ff0b5228e61e8696f303
jdegerickx <jeroen.degerickx@vito.be>
2025-03-12 14:34:50 +0100

    v2 demo notebook part I

---
+++ notebooks/notebook_utils/extractions.py
@@ -0,0 +1,311 @@
+import glob
+import logging
+from pathlib import Path
+from typing import List, Optional, Union
+
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from loguru import logger
+from openeo_gfmap.manager.job_splitters import load_s2_grid
+
+from worldcereal.utils.refdata import gdf_to_points
+
+logging.getLogger("rasterio").setLevel(logging.ERROR)
+
+RDM_ATTRIBUTES = [
+    "sample_id",
+    "ewoc_code",
+    "valid_time",
+    "irrigation_status",
+    "extract",
+    "quality_score_lc",
+    "quality_score_ct",
+    "h3_l3_cell",
+    "geometry",
+]
+
+BANDS = {
+    "S2-L2A": ["B02", "B03", "B04", "B05", "B06", "B07", "B08", "B11", "B12"],
+    "S1-SIGMA0": ["VV", "VH"],
+    "DEM": ["slope", "elevation"],
+    "AGERA5": ["PRECIP", "TMEAN"],
+}
+
+NODATAVALUE = 65535
+
+
+########## FUNCTIONS FOR PREPARING POINT EXTRACTIONS ##########
+
+
+def load_dataframe(df_path: Path) -> gpd.GeoDataFrame:
+    """Load the input dataframe from the given path."""
+
+    if df_path.name.endswith(".geoparquet"):
+        return gpd.read_parquet(df_path)
+    else:
+        return gpd.read_file(df_path)
+
+
+def prepare_samples_dataframe(
+    samples_df_path: Union[str, Path],
+    ref_id: str,
+) -> gpd.GeoDataFrame:
+    """Prepare the samples dataframe for the point extractions.
+
+    Parameters
+    ----------
+    samples_df_path : Union[str, Path]
+        The path to the input dataframe containing the geometries
+        for which extractions need to be done
+    ref_id : str
+        The collection ID.
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        Processed GeoDataFrame containing the samples
+
+    Raises
+    ------
+    ValueError
+        If the input dataframe is missing essential attributes
+    """
+
+    gdf = load_dataframe(Path(samples_df_path))
+
+    # Check presence of essential attributes
+    missing_attributes = set(RDM_ATTRIBUTES) - set(gdf.columns)
+    if missing_attributes:
+        raise ValueError(
+            f"Missing essential attributes in the input dataframe: {missing_attributes}"
+        )
+
+    # Keep essential attributes only
+    gdf = gdf[RDM_ATTRIBUTES]
+
+    # Add ref_id attribute
+    gdf["ref_id"] = ref_id
+
+    # Convert geometries to points
+    n_samples = len(gdf)
+    logger.info(f"Loaded {n_samples} samples from {samples_df_path}")
+    gdf = gdf_to_points(gdf)
+    n_dropped = n_samples - len(gdf)
+    if n_dropped > 0:
+        logger.warning(f"Dropped {n_dropped} samples with invalid geometries.")
+    if len(gdf) == 0:
+        logger.warning("No valid samples left in your dataset.")
+    if len(gdf) > 1000:
+        logger.warning(
+            "More than 1000 samples in your dataset. Extractions will take a while."
+        )
+
+    # Check how many S2 tiles are involved
+    s2_grid = load_s2_grid()
+    gdf = gpd.sjoin(
+        gdf,
+        s2_grid[["tile", "geometry"]],
+        predicate="intersects",
+    ).drop(columns=["index_right"])
+    n_tiles = len(gdf["tile"].unique())
+    logger.info(f"Samples cover {n_tiles} S2 tiles.")
+    if n_tiles > 50:
+        logger.warning(
+            "The number of S2 tiles is high. Extractions will likely consume a lot of credits."
+        )
+
+    return gdf
+
+
+########## FUNCTIONS FOR QUALITY CHECK OF POINT EXTRACTIONS ##########
+
+
+def _apply_band_scaling(array: np.array, bandname: str) -> np.array:
+    """Apply scaling to the band values based on the band name."""
+
+    idx_valid = array != NODATAVALUE
+    array = array.astype(np.float32)
+
+    # No scaling for DEM bands
+    if bandname in BANDS["DEM"]:
+        pass
+    # Divide by 10000 for S2 bands
+    elif bandname.startswith("S2-L2A"):
+        array[idx_valid] = array[idx_valid] / 10000
+    # Convert to dB for S1 bands
+    elif bandname.startswith("S1-SIGMA0"):
+        idx_valid = idx_valid & (array > 0)
+        array[idx_valid] = 20 * np.log10(array[idx_valid]) - 83
+    # Scale meteo bands by factor 100
+    elif bandname.startswith("AGERA5"):
+        array[idx_valid] = array[idx_valid] / 100
+    else:
+        raise ValueError(f"Unsupported band name {bandname}")
+
+    return array
+
+
+def load_point_extractions(extractions_dir: Path, subset=False) -> gpd.GeoDataFrame:
+    """Load point extractions from the given folder.
+
+    Parameters
+    ----------
+    extractions_dir : Path
+        path containing extractions for a given collection
+    subset : bool, optional
+        whether to subset the data to reduce the size, by default False
+
+    Returns
+    -------
+    GeoPandas GeoDataFrame
+        GeoDataFrame containing all point extractions,
+        organized in long format
+        (each row represents a single timestep for a single sample)
+    """
+
+    dfs = []
+    # Get all subfolders
+    zones = [x for x in extractions_dir.iterdir() if x.is_dir()]
+    for zone in zones:
+        tiles = [x for x in zone.iterdir() if x.is_dir()]
+        for tile in tiles:
+            infiles = glob.glob(str(tile / "*.geoparquet"))
+            for infile in infiles:
+                dfs.append(gpd.read_parquet(infile))
+                if subset:
+                    # Only load the first file
+                    break
+
+    if len(dfs) == 0:
+        raise FileNotFoundError(f"No point extractions found in {extractions_dir}")
+
+    return pd.concat(dfs)
+
+
+def get_band_statistics(extractions_dir: Path, subset=False) -> pd.DataFrame:
+    """Get the band statistics for the point extractions.
+    Parameters
+    ----------
+    extractions_dir : Path
+        path to the directory containing the extractions
+    subset : bool, optional
+        whether to subset the data to reduce the size, by default False
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing the band statistics
+    """
+
+    # Load the point extractions
+    gdf = load_point_extractions(extractions_dir, subset=subset)
+
+    # Get the band statistics
+    band_stats = {}
+    for sensor, bands in BANDS.items():
+        for band in bands:
+            if sensor == "DEM":
+                bandname = band
+            else:
+                bandname = f"{sensor}-{band}"
+            if bandname in gdf.columns:
+                # count percentage of nodata values
+                nodata_count = (gdf[bandname] == NODATAVALUE).sum()
+                nodata_percentage = nodata_count / len(gdf) * 100
+                # Apply scaling
+                scaled_values = _apply_band_scaling(gdf[bandname].values, bandname)
+                # discard nodata values
+                scaled_values = scaled_values[scaled_values != NODATAVALUE]
+                # get the statistics and only show 2 decimals
+                band_stats[bandname] = {
+                    "%_nodata": f"{nodata_percentage:.2f}",
+                    "min": f"{np.min(scaled_values):.2f}",
+                    "max": f"{np.max(scaled_values):.2f}",
+                    "mean": f"{np.mean(scaled_values):.2f}",
+                    "std": f"{np.std(scaled_values):.2f}",
+                }
+
+    # Convert to DataFrame
+    stats_df = pd.DataFrame(band_stats).T
+
+    return stats_df
+
+
+def visualize_timeseries(
+    extractions_dir: Path,
+    nsamples: int = 5,
+    band: str = "NDVI",
+    outfile: Optional[Path] = None,
+    sample_ids: Optional[List] = None,
+):
+    """Function to visaulize the timeseries for one band and random or specific samples
+    from an extractions folder.
+
+    Parameters
+    ----------
+    extractions_dir : Path
+        path to the directory containing the extractions
+    nsamples : int, optional
+        number of random samples to visualize, by default 5.
+        Gets overruled if sample_ids are specified.
+    band : str, optional
+        the band to visualize, by default NDVI.
+    outfile : Path, optional
+        path to a file to store the visualization, by default None
+    sample_ids : List, optional
+        sample ids for which the time series needs to be visualized,
+        by default None meaning a random subset will be visualized
+    """
+
+    # Check whether we have a valid band name
+    supported_bands = [
+        f"{k}-{value}" for k, values in BANDS.items() for value in values
+    ]
+    supported_bands.append("NDVI")
+    if band not in supported_bands:
+        raise ValueError(f"Band {band} not found in the extractions dataframe")
+
+    # Load the point extractions
+    gdf = load_point_extractions(extractions_dir)
+
+    # Sample the data
+    if sample_ids is None:
+        sample_ids = gdf["sample_id"].unique()
+        selected_ids = np.random.choice(sample_ids, nsamples, replace=False)
+    else:
+        selected_ids = sample_ids
+
+    fig, ax = plt.subplots()
+
+    for sample_id in selected_ids:
+        sample = gdf[gdf["sample_id"] == sample_id]
+        sample = sample.sort_values("timestamp")
+
+        # Prepare the data to be shown
+        if band == "NDVI":
+            nir = sample.loc[:, "S2-L2A-B08"].values.astype(np.float32)
+            nir[nir == NODATAVALUE] = np.nan
+            red = sample.loc[:, "S2-L2A-B04"].values.astype(np.float32)
+            red[red == NODATAVALUE] = np.nan
+            values = (nir - red) / (nir + red)
+        else:
+            # scale values
+            values = sample.loc[:, band].values
+            values = _apply_band_scaling(values, band)
+            # get rid of nodata values
+            values[values == NODATAVALUE] = np.nan
+
+        # plot
+        ax.plot(sample["timestamp"], values, label=sample_id)
+
+    plt.xlabel("Date")
+    plt.ylabel(band)
+    plt.xticks(rotation=90)
+    ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    plt.tight_layout()
+    plt.show()
+
+    if outfile is not None:
+        plt.savefig(outfile)
+    return
