67f6d40b258164706a575b1b9f963766bb6c72a6
Kristof Van Tricht <kristof.vantricht@vito.be>
2025-06-12 14:44:25 +0200

    Bugfix: clone correct branch

--- tests/pre_test_script.sh
+++ tests/pre_test_script.sh
@@ -25,7 +25,7 @@ PROMETHEO_URL="https://github.com/WorldCereal/prometheo.git"
 
 su - jenkins -c "cd $dir && \
   source venv310/bin/activate && \
-  git clone -b infer $PROMETHEO_URL && \
+  git clone -b inference $PROMETHEO_URL && \
   cd presto-worldcereal || { echo 'Directory not found! Exiting...'; exit 1; } && \
   pip install .
 "
